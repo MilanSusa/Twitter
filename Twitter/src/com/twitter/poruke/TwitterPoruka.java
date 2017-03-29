3a6e019471345c939aca1af76d2b96da808b73a5
MilanSusa <milan_susa@hotmail.com>
2017-03-29 19:41:13 +0200

    Dodata dokumentacija svih atributa i metoda u okviru klase TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,29 +1,76 @@
 package com.twitter.poruke;
 
+/**
+ * Ova klasa predstavlja twitter poruku nekog korisnika
+ * 
+ * @author Milan Susa
+ * @version V1.0
+ */
 public class TwitterPoruka {
+
+	/**
+	 * Ovaj atribut predstavlja korisnika cija je poruka
+	 */
 	private String korisnik;
+
+	/**
+	 * Ovaj atribut predstavlja poruku nekog korisnika
+	 */
 	private String poruka;
 
+	/**
+	 * Ova metoda vraca vrednost promenljive korisnik
+	 * 
+	 * @return korisnik (Vrednost promenljive this.korisnik)
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Ova metoda postavlja vrednost argumenta u promenjivu korisnik
+	 * 
+	 * @param korisnik
+	 *            (Obelezava korisnika koji se unosi)
+	 * @throws java.lang.RuntimeException.class
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || !korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Ova metoda vraca vrednost promenljive poruka
+	 * 
+	 * @return poruka (Vrednost promenjljive this.poruka)
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 
+	/**
+	 * Ova metoda postavlja vrednost argumenta u promenljivu poruka
+	 * 
+	 * @param poruka
+	 *            (Obelezava poruku koja se unosi)
+	 * @throws java.lang.RuntimeException.class
+	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
 
+	/**
+	 * Ova metoda formatirano vraca vrednosti promenljivih korisnik i poruka
+	 * 
+	 * @return Vrednost promenljivih:
+	 *         <ul>
+	 *         <li>this.korisnik</li>
+	 *         <li>this.poruka</li>
+	 *         </ul>
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
