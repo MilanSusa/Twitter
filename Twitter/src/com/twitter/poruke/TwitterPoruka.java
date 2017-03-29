c97b3f1724bb59a43f48359f32846949da6df255
MilanSusa <milan_susa@hotmail.com>
2017-03-29 20:52:11 +0200

    Dodata test klasa TwitterPorukaTest i uradjeni unit testovi

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -35,7 +35,7 @@ public class TwitterPoruka {
 	 * @throws java.lang.RuntimeException.class
 	 */
 	public void setKorisnik(String korisnik) {
-		if (korisnik == null || !korisnik.isEmpty())
+		if (korisnik == null || korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
@@ -57,7 +57,7 @@ public class TwitterPoruka {
 	 * @throws java.lang.RuntimeException.class
 	 */
 	public void setPoruka(String poruka) {
-		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
+		if (poruka == null || poruka.isEmpty() || poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
---
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -0,0 +1,60 @@
+package com.twitter.poruke;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterPorukaTest {
+
+	private TwitterPoruka tp;
+
+	@Before
+	public void setUp() throws Exception {
+		tp = new TwitterPoruka();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikNull() {
+		tp.setKorisnik(null);
+		assertEquals(java.lang.RuntimeException.class, tp.getKorisnik());
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikPrazanString() {
+		tp.setKorisnik("");
+		assertEquals(java.lang.RuntimeException.class, tp.getKorisnik());
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetPorukaNull() {
+		tp.setPoruka(null);
+		assertEquals(java.lang.RuntimeException.class, tp.getPoruka());
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetPorukaPrazanString() {
+		tp.setPoruka("");
+		assertEquals(java.lang.RuntimeException.class, tp.getPoruka());
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetPorukaDugacakString() {
+		tp.setPoruka("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		assertEquals(java.lang.RuntimeException.class, tp.getPoruka());
+	}
+
+	@Test
+	public void testToString() {
+		tp.setKorisnik("Mita");
+		tp.setPoruka("Danas je lep dan");
+		assertEquals("KORISNIK:Mita PORUKA:Danas je lep dan", tp.toString());
+	}
+}
